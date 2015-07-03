33b862295ea52882c0d5f24d04a31666a06b50d6
alyssaamrhein <alyssaanneamrhein@gmail.com>
2015-07-02 22:08:15 -0400

    end of night

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,16 @@
+var myPosition = "";
+var lat = "";
+var lng = "";
 
+navigator.geolocation.getCurrentPosition(success);
 
-navigator.geolocation.getCurrentPosition(function(position) {
-    console.log(position);
-    console.log(position.coords);
-	console.log(position.timestamp);
-	console.log(position.coords.latitude);
-	console.log(position.coords.longitude);
+function success(pos) {
+	console.log(pos)
+};
 
-}, function(positionError) {
-    console.log(positionError);
-});
-
-//var myPosition = " ";
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+var marker = L.marker([51.505, -0.09]).addTo(map);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -22,3 +18,19 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'ahharissa.83ee213e',
     accessToken: 'pk.eyJ1IjoiYWhoYXJpc3NhIiwiYSI6IjQyNTYzNDdhNzgxZTNkMTYwYzg1Zjc5NzFkNjBlZWQyIn0.kyAfNZM16G6srCx76hI09g'
 }).addTo(map);
+
+
+// function(position) {
+//     console.log(position);
+//     console.log(position.coords);
+// 	console.log(position.timestamp);
+// 	console.log(position.coords.latitude);
+// 	console.log(position.coords.longitude);
+// 	position = myPosition;
+// 	lat = position.coords.latitude;
+// 	lng = position.coords.longitude;
+// 	console.log(lat);
+
+// }, function(positionError) {
+//     console.log(positionError);
+//}
